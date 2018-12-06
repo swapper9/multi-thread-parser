a8855c1409e3e5d552a26d3f19085b33fb9fa9b6
swapper9 <32822455+swapper9@users.noreply.github.com>
2018-12-06 21:04:49 +0300

    fixes with queue size and exit condition

--- src/main/java/com/swapper9/parser/MultiThread.java
+++ src/main/java/com/swapper9/parser/MultiThread.java
@@ -1,20 +1,60 @@
 package com.swapper9.parser;
 
+import org.jsoup.Jsoup;
+import org.jsoup.UnsupportedMimeTypeException;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.concurrent.*;
 
 public class MultiThread {
-    static volatile int fileCount = 0;
+    private static volatile int fileCount = 0;
+    private static final BlockingQueue<String> queue = new LinkedBlockingQueue<>();
+
+    static private Document parsePage(String url) {
+        Document doc = null;
+        try {
+            doc = Jsoup.connect(url).get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return doc;
+    }
+
+    static private void saveFiles() {
+        Document doc = null;
+        try {
+            try {
+                if (queue.size()>1){
+                    doc = Jsoup.connect(queue.take()).get();
+                    System.out.println("Queue size: " + queue.size());
+                } else System.exit(1);
+            } catch (InterruptedException | UnsupportedMimeTypeException e) {
+                e.printStackTrace();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String file = doc.outerHtml();
+        String filename = "filename"+(fileCount != 0 ? fileCount : "-0");
+        try(FileWriter fw = new FileWriter("D:/wiki/"+filename+".html")) {
+            fw.write(file);
+            System.out.println("Файл "+filename+" записан потоком " + Thread.currentThread().getName());
+            fileCount--;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
         String startUrl = "https://habr.com/hubs/design/";
-        Reader reader = new Reader();
-        Document doc = reader.parsePage(startUrl);
+        Document doc = parsePage(startUrl);
         Elements links = doc.select("a[href]");
-        BlockingQueue<String> queue = new LinkedBlockingQueue<>(links.size());
+
         for (Element link : links) {
             String absLink = link.attr("abs:href");
             queue.offer(absLink);
@@ -23,22 +63,23 @@ public class MultiThread {
         ExecutorService executor = Executors.newFixedThreadPool(10);
 
         for (int i = 0; i < links.size(); i++) {
-            executor.execute(() -> reader.saveFiles(queue));
-        }
-        if(queue.size() < 1){
-            try {
-                System.out.println("Выключаем executor...");
-                executor.shutdown();
-                executor.awaitTermination(5, TimeUnit.SECONDS);
-            } catch (InterruptedException e) {
-                System.err.println("Задания прерваны...");
-            } finally {
-                if (!executor.isTerminated()) {
-                    System.err.println("Отмена незаконченных заданий...");
+            if (queue.size()>1) {
+                executor.execute(() -> saveFiles());
+            } else {
+                try {
+                    System.out.println("Выключаем executor...");
+                    executor.shutdown();
+                    executor.awaitTermination(5, TimeUnit.SECONDS);
+                } catch (InterruptedException e) {
+                    System.err.println("Задания прерваны...");
+                } finally {
+                    if (!executor.isTerminated()) {
+                        System.err.println("Отмена незаконченных заданий...");
+                    }
+                    executor.shutdownNow();
+                    System.out.println("Работа остановлена.");
+
                 }
-                executor.shutdownNow();
-                System.out.println("Работа остановлена.");
-                
             }
         }
     }
